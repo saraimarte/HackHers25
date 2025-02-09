feb74eb061ed47d77a8e276035c5961c92dd63e8
Catherine Peng <catejaswhy@gmail.com>
2025-02-08 19:38:34 -0500

    f

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+export function SiAddCircleLine(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="1.5" d="M8 12h8m-4 4V8m10 4c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2s10 4.477 10 10"></path></svg>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,6 +21,7 @@ function App() {
           description = ""
         />
       </div>
+      
     </>
   )
 }
