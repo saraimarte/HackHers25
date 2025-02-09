4b724417d1ad9c32a281471e1404f84b833dbb97
Sarai Marte <smartepujols@gmail.com>
2025-02-09 01:52:39 -0500

    020925 react connected with expressjs backend working + get button gets message in console + mongodb connected

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,32 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  async function onClick(){
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    try{
+      const response = await fetch('http://localhost:8080/api/tasks');
+      const data = await response.json();
+      console.log(JSON.stringify(data));
+    } catch(error){
+      console.log("Something went wrong.");
+    }
+  }
+
+  return(
+  <div className = "container">
+      <button className = "task" onClick = {onClick}>Get Tasks</button>
+  </div>
+  );
+
+  //when you call fetch(), the Response object is like a "container" holding metadata and methods to extract the actual data from the server.
 }
 
+
 export default App
+
